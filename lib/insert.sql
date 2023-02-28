ab9849affcf367b01509ed24d5caa6dfa84a64ac
Ayah Elsais <elsais.ayah@gmail.com>
2023-02-28 12:43:18 -0500

    donesies

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES 
+("Mr. Chocolate", 90, "M", "brown", "happy", true),
+("Rowdy", 90, "M", "brown", "happy", true),
+("Tabitha", 90, "M", "brown", "happy", true),
+("Sergeant Brown", 90, "M", "brown", "happy", true),
+("Melissa", 90, "M", "brown", "happy", true),
+("Grinch", 90, "M", "brown", "happy", true),
+("Wendy", 90, "M", "brown", "happy", true),
+(NULL, 90, "M", "brown", "happy", true);
